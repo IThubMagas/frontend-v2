3522f9240799dca25a6e2197acca0b1b3d1d6200
AhmedLyanov <amoshal1997@gmail.com>
2026-04-01 13:48:54 +0300

    sliced components in header

--- widgets/header/ui/index.tsx
+++ widgets/header/ui/index.tsx
@@ -1,16 +1,13 @@
+import Navigation from "./navigation"
 export default function index() {
   return (
     <header className="flex justify-between align-center pt-16 pl-24 pr-24 pb-6">
       <h1 className="text-[#9747FF] font-bold text-4xl">
         <img src="/HRhub.svg" alt="HRhub logo" />
       </h1>
-      <div className="flex">
-        <nav className="flex items-center gap-7">
-          <a className="font-medium text-[16px] hover:text-[#9747FF]" href="">Главная</a>
-          <a className="font-medium text-[16px] hover:text-[#9747FF]" href="">Специалисты</a>
-          <a className="font-medium text-[16px] hover:text-[#9747FF]" href="">О нас</a>
-        </nav>
-        {/* Вот на этой строке ты поставишь кнопку входа */}
+      <div className="flex gap-28">
+       <Navigation/>
+        {/* Вот на этой строке ты вставишь компонент кнопки входа */}
       </div>
     </header>
   )
